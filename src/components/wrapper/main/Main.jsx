b2df3df278394cd5de822ae225d1992fe7374f30
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-13 23:53:06 +0600

    Add prettier for WebStorm:)

--- src/components/wrapper/main/Main.jsx
+++ src/components/wrapper/main/Main.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../../pages/Home";
 import { About } from "../../pages/About";
 import { Contact } from "../../pages/Contact";
-import { Projects } from "../../pages/Projects";
+import { Projects } from "../../pages/projects/Projects.jsx";
 
 export const Main = () => {
 	return (
