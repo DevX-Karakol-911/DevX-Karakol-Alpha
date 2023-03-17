c30819bb32bf3b5fefb174289a2ec2e2e21df540
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-17 15:02:09 +0600

    SwiperCarousel.jsx

--- src/components/carousel/SwiperCarousel.jsx
+++ src/components/carousel/SwiperCarousel.jsx
@@ -152,7 +152,7 @@ export const SwiperCarousel = () => {
 						</div>
 					</SwiperSlide>
 				))}
-				<div className="autoplay-progress" slot="container-end">
+				<div className="autoPlay-progress" slot="container-end">
 					<svg viewBox="0 0 48 48" ref={progressCircle}>
 						<circle cx="24" cy="24" r="20"></circle>
 					</svg>
